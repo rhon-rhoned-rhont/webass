73f15d37b87d9e1050cb0d5bd5890a53d16940ad
Rhon <98303094+rhon-rhoned-rhont@users.noreply.github.com>
2024-09-02 23:54:37 +0530

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('footerButton').addEventListener('click', function() {
+        alert('Didnt i say not to? 😂👈\n\n\n---Welcome to my page---');
+    });
+
+const profileImg = document.getElementsByClassName('profile-img')[0]
+profileImg.addEventListener('mouseenter',()=>{
+    document.body.style.background="url('dog.jpg') repeat ";
+    profileImg.src  = "prof.png";
+})
+
+profileImg.addEventListener('mouseleave',()=>{
+    document.body.style.background="hsl(96, 52%, 58%)";
+    profileImg.src = "dor.jpg";
+})
